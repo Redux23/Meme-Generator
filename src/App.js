ff111d3691e5786205ee19828a96a083f291b9d0
Redux23 <bo.osuagwu@gmail.com>
2020-06-03 09:30:43 +0100

    Added the Header and MemeGenerator tag elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from './component/Header';
+import MemeGenerator from './component/MemeGenerator';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h3>Hello World!</h3>
+      <Header />
+      <MemeGenerator />
      
     </div>
   );
