c81d04b505f207a4502dd78da74b33a3021e7f76
Redux23 <bo.osuagwu@gmail.com>
2020-06-03 10:15:45 +0100

    Added a lifecycle  fetch api method.

--- src/component/MemeGenerator.js
+++ src/component/MemeGenerator.js
@@ -5,9 +5,26 @@ class MemeGenerator extends Component{
     constructor(){
         super()
         this.state = {
+            topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemesImg: []
 
         }
+
+       
     }
+
+    componentDidMount(){
+        fetch("https://api.imgflip.com/get_memes")
+        .then(response => response.json())
+        .then(response => {
+            const {memes} = response.data
+            console.log(memes)
+        })
+    }
+
+
     render(){
         return(
             <h1>Meme Generator section</h1>
